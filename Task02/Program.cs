409b82e890509970d9cbb62fa463c7e2b451d8ee
Администратор <lieber.mann.0@gmail.com>
2021-11-08 12:13:14 +0300

    ADdd corrections to Task 02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,23 +1,17 @@
 ﻿//Даны два числа. Показать большее и меньшее число
 
-int a;
-int b;
-string s;
-
 Console.Write("Введите первое число:  ");
-s = Console.ReadLine();
-a = Convert.ToInt32(s);
+int a = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите второе число:  ");
-s = Console.ReadLine();
-b = Convert.ToInt32(s);
+int b = Convert.ToInt32(Console.ReadLine());;
 
 if (a > b)
     {
     Console.WriteLine("Большее число "+a);
     Console.WriteLine("Меньшее число "+b);
     }
-else
+if (a < b)
     {
      Console.WriteLine("Большее число "+b);
      Console.WriteLine("Меньшее число "+a);
