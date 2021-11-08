849990c28b2eb9cc6621cb5e5fba412b67ef7635
Администратор <lieber.mann.0@gmail.com>
2021-11-09 01:17:54 +0300

    Add Task 04

---
+++ Task04/Program.cs
@@ -0,0 +1,16 @@
+﻿//Найти максимальное из трех чисел
+
+int NumA = new Random().Next(-1000, 1000);
+Console.WriteLine(NumA);
+int NumB = new Random().Next(-1000, 1000);
+Console.WriteLine(NumB);
+int NumC = new Random().Next(-1000, 1000);
+Console.WriteLine(NumC);
+
+int max = NumA;
+
+if (NumB > max) max = NumB;
+if (NumC > max) max = NumC;
+
+Console.WriteLine();
+Console.WriteLine("Максимальное число "+max);
