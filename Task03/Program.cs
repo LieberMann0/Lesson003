48a0ef76356aba07eb8b718fee306dc10bdb2a4d
Администратор <lieber.mann.0@gmail.com>
2021-11-09 00:36:26 +0300

    Add Task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿// По заданному номеру дня недели вывести его название
+
+
+string[] DName = new string[8] {"Необходимо вводить число от 1 до 7", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение"};
+
+Console.Write("Введите номер дня недели: - ");
+int index = Convert.ToInt32(Console.ReadLine());
+
+if (index > 7) Console.WriteLine("Необходимо вводить число от 1 до 7");
+if (index < 0) Console.WriteLine("Необходимо вводить число от 1 до 7");
+
+Console.WriteLine(DName[index]);
