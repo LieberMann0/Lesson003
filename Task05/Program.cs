8bf8d071c67d7a9f9409642ee78c5333c97e2404
Администратор <lieber.mann.0@gmail.com>
2021-11-09 03:22:53 +0300

    Add Task 05

---
+++ Task05/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления значения функции y=f(a)
+
+int F(int a)
+{
+    int y = a;
+    return y;
+}
+
+Console.Write("Введите значение a -  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int result = F(a);
+Console.WriteLine("y = "+result);
