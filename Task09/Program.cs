f8d1ca5d52b08b769e5ba56c8b9060a2f823fe16
Администратор <lieber.mann.0@gmail.com>
2021-11-10 22:43:52 +0300

    Add Task 09

---
+++ Task09/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = a%10;
+Console.Write(result);
