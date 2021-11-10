21320eb8e7a2be9e09c6dc74256a7c089838faef
Администратор <lieber.mann.0@gmail.com>
2021-11-10 18:03:03 +0300

    Add Task 06

---
+++ Task06/Program.cs
@@ -0,0 +1,9 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число - ");
+int a = Console.Read();
+if (a%2 == 0)
+    {
+        Console.WriteLine("Введенное число - четное");
+    }
+else Console.WriteLine("Введенное число - нечетное");
