0d81b6dba305ada80c7d41d8734275044f683232
Администратор <lieber.mann.0@gmail.com>
2021-11-10 23:44:19 +0300

    Add Task 11

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -2,6 +2,5 @@
 
 Console.Write("Введите трехзначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-
 a = a / 10;
 Console.Write(a%10);
---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int num = new Random().Next(10, 99);
+Console.WriteLine(num);
+
+int a = num%10;
+int b = num / 10;
+
+int max;
+
+if (a > b) max = a;
+else max = b;
+
+Console.WriteLine("Наибольшая цифра: "+max);
