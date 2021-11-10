2776a2e55f95a384a8cb40de75f0cae6f3d3bf83
LieberMann0 <92319922+LieberMann0@users.noreply.github.com>
2021-11-10 23:56:46 +0300

    Update Program.cs
    Add correction

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 
-int num = new Random().Next(10, 99);
+int num = new Random().Next(10, 100);
 Console.WriteLine(num);
 
 int a = num%10;
