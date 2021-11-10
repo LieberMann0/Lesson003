bf5569af77d4e77dc32cc5e78e300fcaea218187
Администратор <lieber.mann.0@gmail.com>
2021-11-10 21:37:14 +0300

    Add Task 07

---
+++ Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Показать числа от -N до N
+
+Console.Write("Введите отрицательное число: ");
+int neg = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите положительное число: ");
+int pos = Convert.ToInt32(Console.ReadLine());
+
+int count = (pos - (neg)) +1;
+
+for(int i = 0; i < count; i++, neg++)
+{
+    Console.Write((neg)+"  ");
+}
