c6f5655c66bb63cd92278213ed7d9f3329a9b06f
Администратор <lieber.mann.0@gmail.com>
2021-11-11 01:36:40 +0300

    Add Task 12

---
+++ Task12/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int a = num%10;
+num = num / 10;
+int b = num - num%10;
+
+int c = a + b;
+
+Console.WriteLine(c);
