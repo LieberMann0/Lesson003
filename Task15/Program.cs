e155c191ff7dda72bdeb5096c64c1e6724e42593
Администратор <lieber.mann.0@gmail.com>
2021-11-11 18:28:58 +0300

    Add Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int a = new Random().Next(7, 2001);
+Console.WriteLine(a);
+if ((a%7 == 0) && (a%23 == 0)) Console.WriteLine("Число кратно 7 и 23.");
+if ((a%7 == 0) && (a%23 > 0)) Console.WriteLine("Число кратно только 7.");
+if ((a%7 > 0) && (a%23 == 0)) Console.WriteLine("Число кратно только 23.");
+if ((a%7 > 0) && (a%23 > 0)) Console.WriteLine("Число не кратно данным числам");
