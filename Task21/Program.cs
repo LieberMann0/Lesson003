8556b926cc38d231954111b00c4a1cfc43a3586a
Администратор <lieber.mann.0@gmail.com>
2021-11-11 19:25:20 +0300

    Add Task 21

---
+++ Task21/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа проверяет пятизначное число на палиндром.
+
+string a = Convert.ToString(new Random().Next(10000, 100000));
+Console.WriteLine(a);
+if ((a[0] == a[4]) && (a[1] == a[3])) Console.WriteLine("ПАЛИНДРОМ!");
+else Console.WriteLine("Не палиндром");
