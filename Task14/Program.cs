c7be83d93696c19224e91e2fb8021d444243a522
Администратор <lieber.mann.0@gmail.com>
2021-11-11 18:07:23 +0300

    Add Task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int num = new Random().Next(1, 100001);
+Console.WriteLine(num);
+if (num < 100) Console.WriteLine("Третьей цифры нет.");
+else
+{
+string n = Convert.ToString(num);
+Console.WriteLine("Третья цифра равна - "+(n[2]));
+}
