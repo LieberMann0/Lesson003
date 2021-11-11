72fc4b42ce03ae04ce3f519906358ba75f034d4f
Администратор <lieber.mann.0@gmail.com>
2021-11-11 19:03:24 +0300

    Add Task 17

---
+++ Task17/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+int a = new Random().Next(1, 11);
+Console.WriteLine(a);
+
+int b = new Random().Next(1, 101);
+Console.WriteLine(b);
+
+int aSq = Convert.ToInt32(Math.Pow(a, 2));
+
+if (aSq == b) Console.WriteLine("Второе число является квадратом первого.");
+else Console.WriteLine("Второе число не является квадратом первого.");
