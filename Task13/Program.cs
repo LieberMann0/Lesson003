fe735d3be3a18a58ecdc66d6ee2ff916182637a3
Администратор <lieber.mann.0@gmail.com>
2021-11-13 23:31:51 +0300

    Add correction Task 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,6 +4,6 @@ int a = 13;
 int b = new Random().Next(1, 1001);
 Console.WriteLine(b);
 Console.WriteLine("Кратно ли данное число, числу 13?");
-int result = b%13;
-if (b%13 == 0) Console.WriteLine("Да.");
+int result = b%a;
+if (result == 0) Console.WriteLine("Да.");
 else Console.WriteLine("Нет.  Остаток - "+b%13);
