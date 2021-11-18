6c7cc0565dace0456b76298342f8f370b56edaa4
Администратор <lieber.mann.0@gmail.com>
2021-11-18 18:31:11 +0300

    Add correct Task 12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -10,3 +10,10 @@ int b = num - num%10;
 int c = a + b;
 
 Console.WriteLine(c);
+
+/* Второй вариант
+ 
+string a = Convert.ToString(num);
+Console.WriteLine($"{a[0]}{a[2]}");  Эту строчку можно по-другому записать: Console.WriteLine(a[0] + "" + a[2]);
+                                                                            Console.WriteLine("{0}{1}", a[0], a[2]);
+*/
